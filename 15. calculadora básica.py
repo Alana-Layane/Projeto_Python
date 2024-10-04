e09e457b6294dae046afc8875079e05a9e37d9b9
Alana Layane <alanalayane.of@gmail.com>
2024-10-04 11:11:29 -0300

    Add files via upload

--- 15. calculadora básica.py
+++ 15. calculadora básica.py
@@ -1,49 +1,54 @@
 #Calculadora com escolha do usuário#
+opcao = 0
 
-print("######## MENU DA CALCULADORA #########")
-print("**************CATÁLOGO:**************")
-print("1. soma")
-print("2. subtração")
-print("3. multiplicação")
-print("4. divisão")
-print("5. Resto da divisão")
-print("6. Potencia")
-print("7. sair")
+while True:
 
-opcao = int(input("Escolha sua opção: "))
+    print(" ")
+    print("######## MENU DA CALCULADORA #########")
+    print("**************CATÁLOGO:**************")
+    print("1. soma")
+    print("2. subtração")
+    print("3. multiplicação")
+    print("4. divisão")
+    print("5. Resto da divisão")
+    print("6. Potencia")
+    print("7. sair")
 
-if (opcao) in [1]:
-    valorA = int(input("Digite um valor: "))
-    valorB = int(input("Digite um valor: "))
-    print(f"{valorA} + {valorB} = {valorA + valorB}")
+    opcao = int(input("Escolha sua opção: "))
 
-elif (opcao) in [2]:
-    valorA = int(input("Digite um valor: "))
-    valorB = int(input("Digite um valor: "))
-    print(f"{valorA} - {valorB} = {valorA - valorB}")
+    if (opcao) in [1]:
+        valorA = int(input("Digite um valor: "))
+        valorB = int(input("Digite um valor: "))
+        print(f"{valorA} + {valorB} = {valorA + valorB}")
 
-elif (opcao) in [3]:
-    valorA = int(input("Digite um valor: "))
-    valorB = int(input("Digite um valor: "))
-    print(f"{valorA} x {valorB} = {valorA * valorB}")
+    elif (opcao) in [2]:
+        valorA = int(input("Digite um valor: "))
+        valorB = int(input("Digite um valor: "))
+        print(f"{valorA} - {valorB} = {valorA - valorB}")
 
-elif (opcao) in [4]:
-    valorA = int(input("Digite um valor: "))
-    valorB = int(input("Digite um valor: "))
-    print(f"{valorA} : {valorB} = {valorA / valorB}")
+    elif (opcao) in [3]:
+        valorA = int(input("Digite um valor: "))
+        valorB = int(input("Digite um valor: "))
+        print(f"{valorA} x {valorB} = {valorA * valorB}")
 
-elif (opcao) in [5]:
-    valorA = int(input("Digite um valor: "))
-    valorB = int(input("Digite um valor: "))
-    print(f" O resto da divisão de {valorA} por {valorB} é igual a {valorA // valorB}")
+    elif (opcao) in [4]:
+        valorA = int(input("Digite um valor: "))
+        valorB = int(input("Digite um valor: "))
+        print(f"{valorA} : {valorB} = {valorA / valorB}")
 
-elif (opcao) in [6]:
-    valorA = int(input("Digite um valor: "))
-    valorB = int(input("Digite um valor: "))
-    print(f"{valorA} elevado a {valorB} = {valorA ** valorB}")
+    elif (opcao) in [5]:
+        valorA = int(input("Digite um valor: "))
+        valorB = int(input("Digite um valor: "))
+        print(f" O resto da divisão de {valorA} por {valorB} é igual a {valorA // valorB}")
 
-elif (opcao) in [7]:
-    print("Finalizando...")
+    elif (opcao) in [6]:
+        valorA = int(input("Digite um valor: "))
+        valorB = int(input("Digite um valor: "))
+        print(f"{valorA} elevado a {valorB} = {valorA ** valorB}")
 
-else:
-    print("Essa opção não é permitida, tente outra!")
+    elif (opcao) in [7]:
+        print("Finalizando...")
+        break
+
+    else:
+        print("Essa opção não é permitida, tente outra!")
