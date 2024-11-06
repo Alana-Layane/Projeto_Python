0e580105b449d283a1e50bd81a56acb481e57bdf
Alana Layane <alanalayane.of@gmail.com>
2024-11-06 12:03:07 -0300

    Add files via upload
    finalizando

---
+++ 21. lista for.py
@@ -0,0 +1,10 @@
+#Crie uma lista de 5 números inteiros fernecidos pelo usuário e imprima o maior, o menor e a soma de todos os elementos.
+
+n = int(input("Digite um número: "))
+u = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+e = int(input("Digite um número: "))
+r = int(input("Digite um número: "))
+numeros = [n, u, b, e, r]
+print(numeros)
+
