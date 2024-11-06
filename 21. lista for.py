ddb2ed093a2e51a46a7a54f76e88d30f7c9cd193
Alana Layane <alanalayane.of@gmail.com>
2024-11-06 12:12:16 -0300

    Update 21. lista for.py
    finalizando.

--- 21. lista for.py
+++ 21. lista for.py
@@ -8,3 +8,16 @@ r = int(input("Digite um número: "))
 numeros = [n, u, b, e, r]
 print(numeros)
 
+#Crie uma lista de 5 números inteiros fernecidos pelo usuário e imprima o maior, o menor e a soma de todos os elementos.
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+n3 = int(input("Digite um número: "))
+n4 = int(input("Digite um número: "))
+n5 = int(input("Digite um número: "))
+numeros = [n1, n2, n3, n4, n5]
+max(n1, n2, n3, n4, n5)
+min(n1, n2, n3, n4, n5)
+
+print(f"O maior valor da sua lista é {max} e o menor valor é {min}")
+
